4525a767cbad76b0e11cbf1a5279784b81f3e6a2
Kamil Slowikowski <kslowikowski@gmail.com>
2023-06-07 00:49:22 +0000

    update website

--- website/copy-from-live.sh
+++ website/copy-from-live.sh
@@ -2,14 +2,15 @@
 #
 # After making changes to the live website, we can copy them here.
 
-src=/srv/projects/ircolitis
+src=villani:/srv/projects/ircolitis
 
-command cp -rf $src/{index.html,tables.html,img,css} ./
+command rsync -vr $src/{index.html,img,css} ./
 
 mkdir -p app
 
-command cp -rf $src/app/{index.html,css,ext,js,make-symlinks.sh} ./app/
+command rsync -vr $src/app/{index.html,css,ext,js,make-symlinks.sh} ./app/
 
-mkdir -p de/data
-command cp -rf $src/de/{bundle.js,bundle.js.map,bundle.worker.js,bundle.worker.js.map,index.html,bundle.js.LICENSE.txt,sql-wasm-3896522bd16c305a8c29.wasm,7edd8e1eedab0513cc67d0914307919e.svg} ./de/
+mkdir -p gene-contrasts
+
+command rsync -vr $src/app/{index.html,css,ext,js,make-symlinks.sh} ./gene-contrasts/
 
